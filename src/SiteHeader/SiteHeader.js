d9d1f225494758dbcbabc238fade3fc2130963e9
Stephen Parker <stephenparkhum@outlook.com>
2019-07-19 18:01:03 -0700

    Updating components for a smoother process.

---
+++ src/SiteHeader/SiteHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './SiteHeader.css';
+
+class SiteHeader extends Component {
+    render() { 
+        return ( 
+            <header>
+                <h1>ELF Computing</h1>
+                <h3>Laptops</h3>
+                <h5>Customize your laptop</h5>  
+            </header>  
+         );
+    }
+}
+ 
+export default SiteHeader;
